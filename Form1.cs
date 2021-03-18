b4f8734989c9f648a46771fad5c4c54c5e345237
Nikola <nstipic@etfos.hr>
2021-03-18 10:21:52 +0100

    ...

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV2
+{
+    public partial class Form1 : Form
+    {
+
+        Point ptStart, ptEnd;
+        Graphics graphicsHelper;
+        Pen penHelper;
+        Boolean bMouseDown;
+       
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ptStart = e.Location;
+            bMouseDown = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            bMouseDown = false;  
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (bMouseDown)
+            {
+                ptEnd = e.Location;
+                this.Invalidate();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            graphicsHelper.DrawLine(penHelper, ptStart, ptEnd);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphicsHelper = this.CreateGraphics();
+            penHelper = new Pen(Color.Red, 2);
+            bMouseDown = false;
+        }
+    }
+}
